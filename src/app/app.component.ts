61e6514303a3aa79f9f7345ea49de78a10f7646b
leNicDev <germanelectronix@gmail.com>
2023-05-10 23:23:43 +0200

    support Angular 16

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
-import { environment } from '../environments/environment';
+import { Component } from "@angular/core";
+import { environment } from "../environments/environment";
+import { NgOptimizedImage } from "@angular/common";
 
 @Component({
-  selector: 'hc-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "hc-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-
   appVersion = environment.version;
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,51 +1,51 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { NgHcaptchaModule } from 'ng-hcaptcha';
-import { environment } from '../environments/environment';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { InvisibleComponent } from './invisible/invisible.component';
-import { FormComponent } from './form/form.component';
-import { RouterModule } from '@angular/router';
-import { ProgrammaticallyComponent } from './programmatically/programmatically.component';
+import { AppComponent } from "./app.component";
+import { NgHcaptchaModule } from "ng-hcaptcha";
+import { environment } from "../environments/environment";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { InvisibleComponent } from "./invisible/invisible.component";
+import { FormComponent } from "./form/form.component";
+import { RouterModule } from "@angular/router";
+import { ProgrammaticallyComponent } from "./programmatically/programmatically.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     InvisibleComponent,
     FormComponent,
-    ProgrammaticallyComponent
+    ProgrammaticallyComponent,
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       {
-        path: '',
-        pathMatch: 'full',
-        redirectTo: '/form'
+        path: "",
+        pathMatch: "full",
+        redirectTo: "/form",
       },
       {
-        path: 'form',
-        component: FormComponent
+        path: "form",
+        component: FormComponent,
       },
       {
-        path: 'invisible',
-        component: InvisibleComponent
+        path: "invisible",
+        component: InvisibleComponent,
       },
       {
-        path: 'programmatically',
-        component: ProgrammaticallyComponent
+        path: "programmatically",
+        component: ProgrammaticallyComponent,
       },
     ]),
     FormsModule,
     ReactiveFormsModule,
     NgHcaptchaModule.forRoot({
       siteKey: environment.siteKey,
-      languageCode: 'en'
+      languageCode: "en",
     }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
